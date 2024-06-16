6acce4a5b43794db3817426f3ba56a7c2b44ad45
Flaige <150943265+Flaige@users.noreply.github.com>
2024-06-16 19:40:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-const json = '{"name":"Mango","age":3,"isHappy":true}';
-const obj = JSON.parse(json);
-console.log(obj);
-
-const invalidJson = '{"name":"Mango","age":3,"isHappy":true';
-
-try {
-    const obj = JSON.parse(invalidJson);
-    console.log(obj);
-} catch (error) {
-    console.error("Wystąpił błąd podczas parsowania JSON:");
-    console.error("Nazwa błędu:", error.name);
-    console.error("Wiadomość błędu:", error.message);
+{
+    "name": "Mango",
+    "age": 3,
+    "isHappy": true
 }
 
-console.log("Skrypt działa dalej mimo błędu parsowania JSON.");
